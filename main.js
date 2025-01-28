055667dfa03518f847a0c80a013074e71fdc72f0
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-28 14:25:11 +0100

    Creata funzione che genera email

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ let emails = [];
 let totalEmails = 10; //Questa variabile mi setta il numero di chiamate che andrò a fare
 
 // Creo una funzione per generare le mail
+
+function generateEmails() {
+    for (let i = 0; i < totalEmails; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => response.json()) 
+            .then(data => {
+                emails.push(data.response); // Salva l'email nell'array
+
+                // Se abbiamo raccolto tutte le email, le stampiamo
+                if (emails.length === totalEmails) {
+                    printEmails(); // Funzione per stampare le manil !!! Ancor anon scritta
+                }
+            })
+            .catch(error => console.error("Si è verificato un errore!", error));
+    }};
+
+    // !!! Creo una funzione per stampare le mail in pagina
